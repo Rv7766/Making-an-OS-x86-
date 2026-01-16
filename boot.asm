3d4e42169e2f42f5cce85999d3bfd8d58aaf9a55
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-16 20:25:15 +0530

    Chapter 2.0 Learned to print the first line or sentence to the screen

--- boot.asm
+++ boot.asm
@@ -1,15 +1,40 @@
-mov ah, 0x0e
-mov al, 'A'
+mov ah, 0x0e                                ;# 1)Switch to teletype Mode : mov ah, 0x0e   
+mov al, 'H'
+int 0x10                                    ;# Call BIOS Interrupt 0x10
+
+mov al, 'e'
 int 0x10
 
-mov al, 65
+mov al, 'l'
 int 0x10
 
-mov al, 0x41
+mov al, 'l'
 int 0x10
 
-mov al, 0b1000001
+mov al, 'o'
 int 0x10
+
+mov al, ','
+int 0x10
+
+mov al, 'W'
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+mov al, 'r'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'd'
+int 0x10
+
+mov al, '!'
+int 0x10
+
 
 jmp $
 times 510-($-$$) db 0
