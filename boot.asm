99ad8c3b75f900a026f7d5e6d05e5fbc05caaaea
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 17:09:44 +0530

    V1.1

--- boot.asm
+++ boot.asm
@@ -1,23 +1,26 @@
 [org 0x7c00]
-mov ah, 0x0e
-lea bx, variableName
 
-printString:
-            mov al,[bx]
-            int 0x10
-            inc bx
-            cmp al, 0
-            je exit
-            jmp printString
+mov ah, 0x0e
+mov bx, helloworld
+jmp printout
+
+printout:
+    mov al, [bx]
+
+    cmp al, 0x0
+    je exit
+
+    int 0x10
+    inc bx
+
+    jmp printout
 
 exit:
     jmp $
-variableName:
-        db "Displaying my first String ", 0
 
+helloworld:
+    db "Input What You Want to Input to the Screen.....", 0x0
 
-
+; boot sector
 times 510-($-$$) db 0
-db 0x55,0xaa 
-
-
+db 0x55, 0xaa
