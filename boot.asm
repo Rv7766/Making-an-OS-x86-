d953fbb411076972339a7fc3548cc54e97d3834b
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 15:34:58 +0530

    Chapter 3.1 Tried to print a string to the screen but failed because memomry is offset by 31744 (Which basically means that instead of storing your string from memory location zero 0 its gets stored from 31744 and so on

--- boot.asm
+++ boot.asm
@@ -1,39 +1,13 @@
 mov ah, 0x0e
-mov al, 90
-jmp loop
+mov al,[variableName]
+int 0x10
+jmp $
+variableName:
+        db "Displaying my first String ", 0
 
-loop:
-        int 0x10
-        dec al                                                          
-        cmp al, 'A' - 1                                                   ;#Exiting the loop in assembly:  
-        je exit                                                           ;#    cmp al, 'Z' + 1
-        jmp loop                                                          ;#    je  exit
-
-exit:
-
-    jmp $
 
 
 times 510-($-$$) db 0
 db 0x55,0xaa 
 
-
-;#cmp A, B
-;#
-;#compares A to B,
-;#stores the results by 
-;#setting "flags" (bit
-;#of a certain register)
-;#
-;#
-;# Possible outcomes of the comparision
-;#
-;#Comparison                  Conditional jump instruction
-;#
-;#  A == B                                je
-;#  A != B                                jne
-;#  A  > B                                jg        
-;#  A  < B                                jl
-;#  A >= B                                jge
-;#  A <= B                                jle                     
 
