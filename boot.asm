8e9a4da577e78bc6b5a04368a7a91208b3fe6064
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 18:03:30 +0530

    Version 1.3 You can Enter/Input  infinite number of characters , but as soon as you type '0' or zero , You can not type any more

--- boot.asm
+++ boot.asm
@@ -1,5 +1,7 @@
 [org 0x7c00]
 
+jmp InputFromKeyboard
+
 InputFromKeyboard:
     mov ah, 0                   ;#For inputting
     int 0x16                    ;#For inputting
@@ -7,7 +9,12 @@ InputFromKeyboard:
     mov ah, 0x0e                ;For Displaying
     int 0x10                    ;For Displaying
 
-    
+    cmp al, '0'
+    je exit
+    jmp InputFromKeyboard
+
+exit:
+    jmp $
 ; boot sector
 times 510-($-$$) db 0
 db 0x55, 0xaa
