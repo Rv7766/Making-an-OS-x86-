42200c985a97d661c45f700417c8b01541ae1fa6
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 18:00:35 +0530

    Version 1.1 Learned to type a character and display it then

--- boot.asm
+++ boot.asm
@@ -1,26 +1,14 @@
 [org 0x7c00]
 
-mov ah, 0x0e
-mov bx, helloworld
-jmp printout
+InputFromKeyboard:
+    mov ah, 0                   ;#For inputting
+    int 0x16                    ;#For inputting
 
-printout:
-    mov al, [bx]
+    mov ah, 0x0e                ;For Displaying
+    int 0x10                    ;For Displaying
 
-    cmp al, 0x0
-    je exit
-
-    int 0x10
-    inc bx
-
-    jmp printout
-
-exit:
-    jmp $
-
-helloworld:
-    db "Input What You Want to Input to the Screen.....", 0x0
-
+    
 ; boot sector
 times 510-($-$$) db 0
 db 0x55, 0xaa
+
