fe86eb870672bd5a6b6cbbac2236a33dcd344fb0
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 13:17:04 +0530

    Chapter 2 HomeWork Learned to print all the capital letters of the alphabet from Z to A in reverse

--- boot.asm
+++ boot.asm
@@ -1,11 +1,11 @@
 mov ah, 0x0e
-mov al, 65
+mov al, 90
 jmp loop
 
 loop:
         int 0x10
-        inc al                                                          
-        cmp al, 'Z' + 1                                                   ;#Exiting the loop in assembly:  
+        dec al                                                          
+        cmp al, 'A' - 1                                                   ;#Exiting the loop in assembly:  
         je exit                                                           ;#    cmp al, 'Z' + 1
         jmp loop                                                          ;#    je  exit
 
