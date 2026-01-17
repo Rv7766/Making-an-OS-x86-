9d042c20b924bc7acd0a24e74d73a70da309be21
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 13:00:34 +0530

    Chapter 2.6 Learned to use conditional statements and loop , When the condition is false or 0

--- boot.asm
+++ boot.asm
@@ -1,12 +1,35 @@
-mov ah, 0x0e
-mov al, 65
-
-loop:
-        int 0x10
-        inc al
-        jmp loop
+mov bx, 7
+cmp bx, 9
+je loop
 
 jmp $
 
+loop:
+        mov ah, 0x0e
+        mov al, 'X'
+        int 0x10
+
+
 times 510-($-$$) db 0
 db 0x55,0xaa 
+
+
+;#cmp A, B
+;#
+;#compares A to B,
+;#stores the results by 
+;#setting "flags" (bit
+;#of a certain register)
+;#
+;#
+;# Possible outcomes of the comparision
+;#
+;#Comparison                  Conditional jump instruction
+;#
+;#  A == B                                je
+;#  A != B                                jne
+;#  A  > B                                jg        
+;#  A  < B                                jl
+;#  A >= B                                jge
+;#  A <= B                                jle                     
+
