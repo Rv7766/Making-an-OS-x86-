04995f9c7e3c70a97f9207a855650c2f4926b78b
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-17 12:38:58 +0530

    Chapter 2.5 Learned to print all the alphabets or symbols represented by ascii code

--- boot.asm
+++ boot.asm
@@ -1,25 +1,12 @@
 mov ah, 0x0e
 mov al, 65
-int 0x10
 
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
-
-inc al
-int 0x10
+loop:
+        int 0x10
+        inc al
+        jmp loop
 
 jmp $
+
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55,0xaa 
