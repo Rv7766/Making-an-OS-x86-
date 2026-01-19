0d5298b5ebccf40be0a6aaf8085f096d98929084
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-19 13:14:54 +0530

    Version 1.5
    Learned to print a string and then take an input string of 18 charactor length and then print that string to the screen
    
    [Bug]:Unable to figure out why the first charactor of the input string is not printing

--- boot.asm
+++ boot.asm
@@ -1,32 +1,62 @@
 [org 0x7c00]
+mov ah, 0x0e
+lea bx, variableName1
+jmp printString
 
-jmp InputFromKeyboard
+printString:
+            mov al,[bx]
+            int 0x10
+            inc bx
+            cmp al, 0
+            je reloadbx
+            jmp printString
 
-InputFromKeyboard:
-    mov ah, 0                   ;#For inputting
-    int 0x16                    ;#For inputting
-
-    mov ah, 0x0e                ;For Displaying
-    int 0x10                    ;For Displaying
-
-    cmp al, '0'
-    je press7keytest
-    jmp InputFromKeyboard
-
- 
-
-
-press7keytest:
-
-                mov ah, 0
-                int 0x16
-                
-                cmp al, '7'
-                je InputFromKeyboard
-                jmp press7keytest
+reloadbx:
+            lea bx,buffer
+            jmp InputString
 
 
-; boot sector
+InputString:
+            mov ah, 0
+            int 0x16
+            mov [bx],al
+            mov ah, 0x0e
+            int 0x10
+
+            inc bx
+            cmp al, 13
+            je reloadbx2
+            jmp InputString
+
+
+reloadbx2:
+            mov bx,buffer
+            mov ah, 0x0e
+            mov al, 13
+            int 0x10
+
+            jmp printString2
+
+printString2:
+            mov al,[bx]
+            int 0x10
+            cmp al, 0
+            je exit
+            inc bx
+            jmp printString2
+
+
+exit:
+    mov al, buffer
+    jmp $
+
+variableName1:
+        db "Input your String in 18 charactors...... ",0
+
+buffer:
+    times 18 db 0
+
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55,0xaa 
+
 
