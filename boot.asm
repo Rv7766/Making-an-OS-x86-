43ac697b40b4c32e1fb4bbcfde6965fbed8c11f2
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-21 20:06:23 +0530

    Chapter 5.0 Learned to write a letter 512 times in 512 bytes after the boot sector and then how to load it to memory and how to print it to the screen

--- boot.asm
+++ boot.asm
@@ -1,62 +1,34 @@
 [org 0x7c00]
+mov [diskNum], dl    ;# moving the drive number from which we booted from the register dl to pointer diskNum
+
+mov ah, 2            
+mov al, 1            ; number of sectors  from which you want to read
+mov ch, 0            ; cylinder number from which you want to read
+mov cl, 2            ; sector number from  you want to read
+mov dh, 0            ; head number from which you want to read
+mov dl, [diskNum]    ; the drive number that we saved in the variable or pointer diskNum
+
+
+
+push ax              ;pushing ax , so that we can move in zero to it 
+mov ax, 0            ; moving in zero or 0 to ax so that we can move zero to ex
+mov es, ax           ; moving ax with zero or 0 to es because we can't directly move values to es i guess
+
+                     
+                     ;setting up the pointer where we want to load up our sector im memory........
+pop ax               ; pointer = es * 16 + bx .....................................................
+mov bx, 0x7e00       ;so if we want pointer = 0x7e00
+int 0x13             ;we need 'es' to be zero annd 'bx' to be '0x7e00' as the offset 16 bit value
+                     ;So then pointer = es (0) * 16 + bx (0x7e00) and pointer becomes 0x7e00
 mov ah, 0x0e
+mov al, [0x7e00]
+int 0x10
 
-lea bx, variableName1
-jmp printString
-
-printString:
-            mov al,[bx]
-            int 0x10
-            inc bx
-            cmp al, 0
-            je reloadbx
-            jmp printString
-
-reloadbx:
-            mov bx, 0
-            jmp InputString
-
-
-InputString:
-            mov ah, 0
-            int 0x16
-            mov ch,al
-            cmp ch, 13
-            je reloadbx2
-            push cx
-            mov ah, 0x0e
-            int 0x10
-
-            inc bx
-            jmp InputString
-
-
-reloadbx2:
-
-            jmp printString2
-
-printString2:
-            
-                pop cx
-                mov al, ch
-                mov ah, 0x0e
-                int 0x10
-
-                cmp bx, 0
-                je exit
-
-                dec bx
-                jmp printString2
-
-
-exit:
-    jmp $
-
-variableName1:
-        db "Input your String in 18 charactors...... ",0
-
-
+jmp $
 times 510-($-$$) db 0
-db 0x55,0xaa 
+db 0x55,0xaa                        ;# Magic booting number at the end of bootsector(512B) 
+times 512 db 'A'               ;# writing 'A' in the next 512 bytes
 
+diskNum:                            ; Reversing space for diskNum pointer of 1 byte
+     db 0
 
