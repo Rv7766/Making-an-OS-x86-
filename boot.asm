7a87ffe42ab5c7d60b119ce4d849c1352009fe82
Amit Kumar Mahato <amit828201@gmail.com>
2026-01-21 16:05:41 +0530

    Chapter 4 Homework
    Learned to push a string to the stack and then printed that string to the screen by popping , Which prints the string in reverse .For example "AMIT" will be printed as "TIMA"

--- boot.asm
+++ boot.asm
@@ -1,38 +1,62 @@
-mov bp, 0x8000
-mov sp, bp
-mov ah, 'T'
-push ax
-
-mov ah, 'I'
-push ax
-
-mov ah, 'M'
-push ax
-
-mov ah, 'A'
-push ax
-
-pop ax
-mov al, ah
+[org 0x7c00]
 mov ah, 0x0e
-int 0x10
 
-pop ax
-mov al,ah
-mov ah, 0x0e
-int 0x10
+lea bx, variableName1
+jmp printString
 
-pop ax
-mov al, ah
-mov ah, 0x0e
-int 0x10
+printString:
+            mov al,[bx]
+            int 0x10
+            inc bx
+            cmp al, 0
+            je reloadbx
+            jmp printString
 
-pop ax
-mov al, ah
-mov ah, 0x0e
-int 0x10
+reloadbx:
+            mov bx, 0
+            jmp InputString
 
 
-jmp $
+InputString:
+            mov ah, 0
+            int 0x16
+            mov ch,al
+            cmp ch, 13
+            je reloadbx2
+            push cx
+            mov ah, 0x0e
+            int 0x10
+
+            inc bx
+            jmp InputString
+
+
+reloadbx2:
+
+            jmp printString2
+
+printString2:
+            
+                pop cx
+                mov al, ch
+                mov ah, 0x0e
+                int 0x10
+
+                cmp bx, 0
+                je exit
+
+                dec bx
+                jmp printString2
+
+
+exit:
+    jmp $
+
+variableName1:
+        db "Input your String in 18 charactors...... ",0
+
+
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55,0xaa 
+
+
